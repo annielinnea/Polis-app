9d431c272b3c9bed5f7c278b86eb96e851d12be1
LiamSG02 <liamswansson@gmail.com>
2021-09-24 14:34:36 +0200

    slidehowKinda

--- polis_app/src/App.js
+++ polis_app/src/App.js
@@ -4,6 +4,8 @@ import './components/Gps/gps.jsx';
 import DataFetching from './components/DataFetching';
 import SearchFunction from './components/SearchFunction';
 import Navbar from "./components/Navbar/Navbar";
+import './components/SlideShow'
+import Slideshow from './components/SlideShow';
 
 function App() {
   return (
@@ -12,7 +14,12 @@ function App() {
     
     <div className="App">
       <Navbar />
+<<<<<<< Updated upstream
 +      <header className="App-header">
+=======
+      <header>
+        <Slideshow />
+>>>>>>> Stashed changes
       </header>
 
       <div className="PostItems">
