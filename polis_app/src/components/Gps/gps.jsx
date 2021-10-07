a8fc18d31230ea36064e20f214d1bd075dbedfd4
Linnea <linnea.1357@gmail.com>
2021-10-07 09:13:22 +0200

    Update gps.jsx

--- polis_app/src/components/Gps/gps.jsx
+++ polis_app/src/components/Gps/gps.jsx
@@ -1,11 +1,5 @@
-
-var options = {
-   enableHighAccuracy: true,
-  timeout: 5000,
- maximumAge: 0
-};
-
-
+import React from 'react';
+import globalValues from '../../globalValues';
 
 
 //Get users current position//
@@ -27,16 +21,58 @@ var options = {
   console.warn(`ERROR(${err.code}): ${err.message}`);
 }
 
+navigator.geolocation.getCurrentPosition(success, error, options);
 
-class GPS extends React.Component {
-  constructor(props){
-  super(props)
-  this.state = {
-   events: []
-  };
+class GPS extends React.Component{
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      events: [],
+    }
+
+  }
+
+
+componentDidUpdate() {
+
 }
 
+render() {
+return(
+  <div className="event-items">
+  <ui>
+    {/* //creates cards with map() */}
+    {this.state.events.filter(event => event.gps.includes([categoryName])).map(event => (
+      <ol className="eventOL">
+        <div
+          key={event.id}
+          className="event-item"
+        >
+          <img
+            class="postImg"
+            alt="Polis Scene"
+          ></img>
+          <p className="event-title">
+            <a href={"https://polisen.se/" + event.url}>
+              {event.name.split(',').slice(1)}
+            </a>
+          </p>
+          <p className="event-date">
+            {event.datetime}
+          </p>
+          <br />
+          <p className="event-summary">
+            {event.summary}
+          </p>
+        </div>
+      </ol>
+    ))
+    }
+  </ui>
+</div>
+)
 
+}
 
-
-
+}
