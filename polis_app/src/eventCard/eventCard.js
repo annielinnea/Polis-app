1f835493efb8822886b99525cb12c4af0cdbf6f5
LiamSG02 <liamswansson@gmail.com>
2021-10-08 09:32:20 +0200

    Revert "update"
    This reverts commit 2be026538f83a77b6e6ce4378fdbfb2448ebae5f.

--- polis_app/src/eventCard/eventCard.js
+++ polis_app/src/eventCard/eventCard.js
@@ -2,7 +2,6 @@ import React from 'react'
 import globalValues from '../globalValues'
 import { checkboxList, categoryName } from '../components/Navbar/Checkbox'
 import filtering from './filter'
-import GPS from '../components/Gps/gps'
 
 let events = [];
 let query = 'onLoad'
@@ -103,7 +102,6 @@ class EventCard extends React.Component {
                   key={event.id}
                   className="event-item"
                 >
-                  <GPS apiX={event.api.split(',').slice(1)} apiY={"23"} />
                   <img
                     class="postImg"
                     alt="Polis Scene"
@@ -113,7 +111,6 @@ class EventCard extends React.Component {
                       {event.name.split(',').slice(1)}
                     </a>
                   </p>
-                  <p>{event.location.gps} </p>
                   <p className="event-date">
                     {event.datetime}
                   </p>
